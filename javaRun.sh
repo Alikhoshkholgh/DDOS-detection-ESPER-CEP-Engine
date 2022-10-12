bee5c844e1e81d42971884087ee40c49f6e8411a
Ali KhoshKholgh <45786220+Alikhoshkholgh@users.noreply.github.com>
2022-10-12 01:20:40 -0700

    Update javaRun.sh

--- javaRun.sh
+++ javaRun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+#-------------------------------- you need to change "path" variable to the directory that your project is placed in.
 path="/opt/service"
 
 dependency_list="$path/jarDependency/hamcrest-core-1.1.jar:$path/jarDependency/junit-4.10.jar:$path/jarDependency/antlr4-runtime-4.7.2.jar:$path/jarDependency/commons-compiler-3.1.0.jar:$path/jarDependency/esper-common-8.7.0.jar:$path/jarDependency/esper-compiler-8.7.0.jar:$path/jarDependency/esper-runtime-8.7.0.jar:$path/jarDependency/janino-3.1.0.jar:$path/jarDependency/log4j-1.2.17.jar:$path/jarDependency/slf4j-log4j12-1.7.30.jar:$path/jarDependency/slf4j-api-1.7.30.jar:$path/jarDependency/esper-7.1.0.jar:$path/jarDependency/json-20210307.jar:./jarDependency/sqlite-jdbc-3.7.2.jar" 
